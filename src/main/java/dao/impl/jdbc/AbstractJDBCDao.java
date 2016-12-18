4639b22350c4cb8431c3766ee8262ee4167da59c
Vladimir Apraxin <turbo.headcrab.666@gmail.com>
2016-12-18 19:38:40 +0300

    Not-working JDBC implemented

--- src/main/java/dao/impl/jdbc/AbstractJDBCDao.java
+++ src/main/java/dao/impl/jdbc/AbstractJDBCDao.java
@@ -1,9 +1,13 @@
 package dao.impl.jdbc;
 
+import dao.DaoFactory;
 import dao.GenericDao;
 import dao.Identifiable;
+import dao.PersistException;
 
 import java.io.Serializable;
+import java.sql.*;
+import java.util.List;
 
 /**
  * Created by Apraxin Vladimir on 18.12.16.
@@ -14,6 +18,9 @@ import java.io.Serializable;
  * @param <PK> тип первичного ключа
  */
 public abstract class AbstractJDBCDao<T extends Identifiable<PK>, PK extends Serializable> implements GenericDao<T, PK> {
+
+    private Connection connection;
+    private DaoFactory<Connection> parentFactory;
 
     /**
      * Возвращает sql запрос для получения всех записей.
@@ -42,4 +49,59 @@ public abstract class AbstractJDBCDao<T extends Identifiable<PK>, PK extends Ser
      * DELETE FROM [Table] WHERE id= ?;
      */
     public abstract String getDeleteQuery();
+
+    /**
+     * Разбирает ResultSet и возвращает список объектов соответствующих содержимому ResultSet.
+     */
+    protected abstract List<T> parseResultSet(ResultSet resultSet) throws PersistException;
+
+    /**
+     * Устанавливает аргументы insert запроса в соответствии со значением полей объекта object.
+     */
+    protected abstract void prepareStatementForInsert(PreparedStatement preparedStatement, T object)
+        throws PersistException;
+
+    /**
+     * Устанавливает аргументы update запроса в соответствии со значением полей объекта object.
+     */
+    protected abstract void prepareStatementForUpdate(PreparedStatement preparedStatement, T object)
+        throws PersistException;
+
+    @Override
+    public T add(PK primaryKey) throws PersistException {
+        return null;
+    }
+
+    @Override
+    public T persist(T object) throws PersistException {
+        return null;
+    }
+
+    @Override
+    public T getByPk(PK primaryKey) throws PersistException {
+        return null;
+    }
+
+    @Override
+    public void update(T object) throws PersistException {
+
+    }
+
+    @Override
+    public void delete(T object) throws PersistException {
+
+    }
+
+    @Override
+    public List<T> getAll() throws PersistException {
+        List<T> list;
+        String sql = getSelectQuery();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            ResultSet resultSet = preparedStatement.executeQuery();
+            list = parseResultSet(resultSet);
+        } catch (Exception e) {
+            throw new PersistException(e);
+        }
+        return list;
+    }
 }
